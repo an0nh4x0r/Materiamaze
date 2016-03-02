1262fc8f79229e78d4edb2a684050b4b0495b30e
an0nh4x0r <swappp7singh@gmail.com>
2016-03-02 16:48:30 +0530

    Forgot password, wrong login popup ... UPDATED!!

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,7 +6,8 @@
         button_text = document.getElementById("password_button_icon"),
         passwordInputField = document.getElementById("password"),
         usernameInputField = document.getElementById("username"),
-        login = document.getElementById("login_button");
+        login = document.getElementById("login_button"),
+        forgot_password_button = document.getElementById("forgot_password_button");
 
     button.onclick = function () {
         if (passwordInputField.type === "password"){
@@ -23,12 +24,31 @@
     /*This section is to check password provided by user*/
 
     login.onclick = function () {
-        if(usernameInputField.value === "an0nh4x0r" && passwordInputField.value === "material_design"){
+        if(usernameInputField.value.trim() === "an0nh4x0r" && passwordInputField.value.trim() === "material_design"){
             window.location = window.location.pathname.replace("login", "profile");
         } else {
-            swal('WTF !!!', "Username = 'an0nh4x0r', Password = 'password'", "error");
+            swal({
+                type                :   "error",
+                title               :   "Oops..!",
+                showConfirmButton   :   false,
+                timer               :   1300,
+                html                :   "<em>Wrong Username or password</em>"
+            });
         }
     };
+
+    /*Section end*/
+
+    /*This section is dedicated to forgot password*/
+
+        forgot_password_button.onclick = function () {
+            swal({
+                title               :   'Forgot Password ?',
+                html                :   '<label for="email" style="font-size: 15px; font-style: italic;">Email Address: </label></label><input id="email" />',
+                confirmButtonColor  :   '#2196F3',
+                confirmButtonText   :   'Submit'
+            });
+        };
 
     /*Section end*/
 
