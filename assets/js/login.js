7662afc931a25e00af340eacc5be02a09d7c152c
an0nh4x0r <swappp7singh@gmail.com>
2016-03-12 22:03:35 +0530

    Applied pattern/regex in the mobile number field now only number can be entered into the mobile number field

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,14 +1,15 @@
 (function () {
-    var i_show_password = document.getElementById("visibility_icon"),
+    var i_show_password = document.getElementById("visibility"),
+        i_show_password_icon = document.getElementById("visibility_icon"),
         password = document.getElementById("password");
 
     var check = function () {
         if (password.type === "password"){
             password.type = "text";
-            i_show_password.textContent = "visibility_off"
+            i_show_password_icon.textContent = "visibility_off"
         } else if (password.type === "text"){
             password.type = "password";
-            i_show_password.textContent = "visibility";
+            i_show_password_icon.textContent = "visibility";
         }
     };
 
