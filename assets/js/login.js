46791c93d824c6d4d47feff4af5913da910897b0
an0nh4x0r <swappp7singh@gmail.com>
2016-03-30 01:31:43 +0530

    Cleaned jQuery written at the bottom of the login.html page... exported it to login.js file!

--- assets/js/login.js
+++ assets/js/login.js
@@ -17,3 +17,27 @@
     i_show_password.addEventListener("click", check, false);
 
 }());
+
+(function(){
+
+    var forgot_password_card = document.getElementById('forgot_password_card'),
+        back_to_login_card = document.getElementById('back_to_login_page'),
+        forgot_password_button = document.getElementById('forgot_password_button'),
+        back_to_login_page = document.getElementById('back_to_login_page'),
+        login_card = document.getElementById('login_card');
+    $(forgot_password_card).hide();
+    $(back_to_login_card).hide();
+    $(forgot_password_button).click(function () {
+        $(login_card).hide();
+        $(forgot_password_button).hide();
+        $(forgot_password_card).fadeIn();
+        $(back_to_login_page).fadeIn();
+    });
+    $(back_to_login_page).click(function () {
+        $(forgot_password_card).hide();
+        $(back_to_login_page).hide();
+        $(login_card).fadeIn();
+        $(forgot_password_button).fadeIn();
+    });
+
+}());
